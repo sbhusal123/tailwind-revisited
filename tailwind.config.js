605e778e429b1bacea5c6a4b19ff659809dd1068
sbhusal123 <suryabhusal11@gmail.com>
2024-08-04 00:37:39 +0545

    tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    // NOTE: note the directory, as we are serving from public directory so it must be added here
     "./public/**/*.{html,js}",
   ],
   theme: {
